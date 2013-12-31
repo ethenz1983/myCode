443166f6895601bdce82750f7db4c1cc2e61b07a
ethenz1983 <ethenz@163.com>
2013-12-31 10:24:39 +0800

    - 创建项目

---
+++ MyCode/MyCode/MyCodeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyCodeAppDelegate.h
+//  MyCode
+//
+//  Created by Ethen on 13-12-31.
+//  Copyright (c) 2013年 EthenZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyCodeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyCode/MyCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyCode
+//
+//  Created by Ethen on 13-12-31.
+//  Copyright (c) 2013年 EthenZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyCodeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyCodeAppDelegate class]));
+    }
+}
